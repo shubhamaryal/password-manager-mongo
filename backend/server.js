7de153e7fa8ae3cf4544d3f26407760eeae2a5d0
shubhamaryal <aryalshubham62@gmail.com>
2025-03-05 18:50:32 +0545

    made apis

--- backend/server.js
+++ backend/server.js
@@ -28,20 +28,33 @@ client.connect();
 // Get all the passwords
 app.get("/", async (req, res) => {
   const db = client.db(dbName);
-  const collection = db.collection("documents");
+  const collection = db.collection("passwords");
   const findResult = await collection.find({}).toArray();
   res.json(findResult);
 });
 
 // Save a password
 app.post("/", async (req, res) => {
+  const password = req.body;
   const db = client.db(dbName);
-  const collection = db.collection("documents");
-  const findResult = await collection.find({}).toArray();
-  res.send(req.body);
+  const collection = db.collection("passwords");
+  const findResult = await collection.insertOne(password);
+  res.send({ success: true, result: findResult });
 });
 
-// Delete a password
+// Delete a password by id
+app.delete("/", async (req, res) => {
+  const password = req.body;
+  const db = client.db(dbName);
+  const collection = db.collection("passwords");
+  const findResult = await collection.deleteOne(password);
+  res.send({ success: true, result: findResult });
+});
+
+// in get req, we can see all the passwords from the DB 
+// in post req, we can same the password in the DB
+// in delete req, we can delete the password in the DB
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
